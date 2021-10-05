219e24d0848cba6700b0d01edadefff93722f277
정혜일 <hyelie@postech.ac.kr>
2021-10-05 22:29:59 +0900

    first commit

---
+++ acmicpc/100.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, int> psi;
+
+void solve();
+
+int main(void) {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+	int test;
+	cin >> test;
+	while (test--) {
+		solve();
+	}
+	return 0;
+}
+
+void solve() {
+	cout << "hello, world!" << endl;
+
+	return;
+}
---
+++ template_single.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, int> psi;
+
+void solve();
+
+int main(void) {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+	solve();
+	return 0;
+}
+
+void solve() {
+
+
+	return;
+}
