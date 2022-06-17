5ad7294b689f83bfba3d6265102f345d017117a4
정혜일 <hyelie@postech.ac.kr>
2022-06-17 11:24:45 +0000

    22.06.17. 리틀 프렌즈 사천성.cp

---
+++ programmers/graph/DFS BFS backtrack/lv3 little friends.cpp
@@ -0,0 +1,93 @@
+// 리틀 프렌즈 사천성
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef pair<pii, pii> ppiipii;
+string answer = "IMPOSSIBLE";
+
+vector<string> boards;
+
+bool checkRow(int rfrom, int rto, int c, int cha){
+    int end = max(rfrom, rto), start = min(rfrom, rto);
+    for(int r = start; r<=end; r++){
+        if(boards[r][c] == cha || boards[r][c] == '.') continue;
+        return false;
+    }
+    return true;
+}
+
+bool checkCol(int cfrom, int cto, int r, int cha){
+    int end = max(cfrom, cto), start = min(cfrom, cto);
+    for(int c = start; c<=end; c++){
+        if(boards[r][c] == cha || boards[r][c] == '.') continue;
+        return false;
+    }
+    return true;
+}
+
+// c : 시작/도착 글자
+// from/to : 출발/도착 좌표
+bool canReach(pii from, pii to, char c){
+    if(checkRow(from.first, to.first, from.second, c) && checkCol(from.second, to.second, to.first, c)) return true;
+    if(checkRow(from.first, to.first, to.second, c) && checkCol(from.second, to.second, from.first, c)) return true;
+    return false;
+}
+
+void backtrack(int cur_depth, int max_depth, vector<vector<pii>>& word_coordinate, vector<bool>& visited, string cur_str){
+    if(cur_depth == max_depth && answer == "IMPOSSIBLE"){
+        answer = cur_str;
+        return;
+    }
+    
+    for(int i = 0; i<word_coordinate.size(); i++){
+        pii from = word_coordinate[i][0];
+        pii to = word_coordinate[i][1];
+        char cur_char = boards[from.first][from.second];
+        if(!visited[i] && canReach(from, to, cur_char)){
+            visited[i] = true;
+            boards[from.first][from.second] = '.';
+            boards[to.first][to.second] = '.';
+            
+            backtrack(cur_depth + 1, max_depth, word_coordinate, visited, cur_str + cur_char);
+            
+            visited[i] = false;
+            
+            boards[from.first][from.second] = cur_char;
+            boards[to.first][to.second] = cur_char;
+        }
+    }
+}
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+string solution(int m, int n, vector<string> board) {
+    boards = board;
+    map<char, vector<pii>> mapper;
+    for(int r = 0; r<m; r++){
+        for(int c = 0; c<n; c++){
+            if(board[r][c] == '*' || board[r][c] == '.') continue;
+            mapper[board[r][c]].push_back({r, c});
+        }
+    }
+    
+    vector<vector<pii>> word_coordinate;
+    for(auto [k, v] : mapper){
+        word_coordinate.push_back(v);
+    } // word_coordinate[i][0] : 첫 좌표, [1] : 두번째 좌표. word_coordinate는 오름차순 정렬.
+    vector<bool> visited(word_coordinate.size(), false);
+    
+    backtrack(0, word_coordinate.size(), word_coordinate, visited, "");
+    
+    
+    return answer;
+}
+
+/*
+경로가 1번 꺾여야 함은 어떻게 알까?
+무조건 ㄱ 또는 ㄴ자가 되어야 한다. 그 경로에 다른 무언가가 있다면 없애지 못하는 것.
+*/
