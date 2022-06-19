fd98208d7d8de0da4f364d81bdcf50654f97ad1b
정혜일 <hyelie@postech.ac.kr>
2022-06-19 10:58:31 +0000

    22.06.19. 몸짱 트레이너 라이언의 고민 solve

--- programmers/brute-force/lv3 trainer lion.cpp
+++ programmers/brute-force/lv3 trainer lion.cpp
@@ -8,69 +8,8 @@
 using namespace std;
 typedef pair<int, int> pii;
 
-int getDist(pii&a, pii&b){
+int getDist(pii a, pii b){
     return abs(a.first - b.first) + abs(a.second - b.second);
-}
-
-// return {r, c} in range
-bool inRange(int r, int c, int n){
-    if(0 <= r && r < n && 0 <= c && c < n) return true;
-    else return false;
-}
-
-// cur_point로부터 거리가 D인 모든 점
-vector<pii> getDistDPoints(int D, pii cur_point, int n){
-    set<pii> result;
-    pii top = {cur_point.first - D, cur_point.second};
-    pii bottom = {cur_point.first + D, cur_point.second};
-    
-    int dr[2] = {1, 1};
-    int dc[2] = {1, -1};
-    
-    for(int i = 0; i<=D; i++){
-        for(int lr = 0; lr < 2; lr++){
-            pii p = {top.first + i * dr[lr], top.second + i * dc[lr]};    
-            if(inRange(p.first, p.second, n)) result.insert(p);
-            
-            p = {bottom.first - i * dr[lr], bottom.second - i * dc[lr]};
-            if(inRange(p.first, p.second, n)) result.insert(p);
-        }
-    }
-    
-    vector<pii> to_v(result.begin(), result.end());
-    return to_v;
-}
-
-bool backtrack(int cur_depth, int max_depth, int target_dist, vector<pii>& results, vector<pii> cur_able_points, int n){
-    if(cur_depth == max_depth-1){
-        return true;
-    }
-    
-    for(int i = 0; i<cur_able_points.size(); i++){
-        pii next_point = cur_able_points[i];
-        results[cur_depth] = next_point;
-        
-        vector<pii> dist_d_points = getDistDPoints(target_dist, next_point, n);
-        vector<pii> next_able_points;
-        
-        for(int dpi = 0; dpi < dist_d_points.size(); dpi++){
-            bool isAllDistOverD = true;
-            for(int cdi = 0; cdi <= cur_depth; cdi++){
-                if(getDist(results[cdi], dist_d_points[dpi]) < target_dist){
-                    isAllDistOverD = false;
-                    break;
-                }
-            }
-            if(isAllDistOverD) next_able_points.push_back(dist_d_points[dpi]);
-        }
-        
-        if(next_able_points.size() == 0) return false;
-        else{
-            bool flag = backtrack(cur_depth+1, max_depth, target_dist, results, next_able_points, n);
-            if(flag) return true;
-        }
-    }
-    return false;
 }
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
@@ -92,6 +31,7 @@ int solution(int n, int m, vector<vector<int>> timetable) {
     // 총 50번 탐색
     // 한 번의 탐색에 이전까지 둔 모든 locker를 봄 - 50번
     // n^5인 듯?
+    // 다시 한 번 생각을 바꿔서, 'dist'가 주어졌을 때 dist를 유지하면서 최대로 넣을 수 있는 인원을 가져와 보자
     int max_overlap = *max_element(times.begin(), times.end());
     
     int chessboard_num = n * n / 2 + n % 2; // chessboard처럼 채울 경우
@@ -99,17 +39,37 @@ int solution(int n, int m, vector<vector<int>> timetable) {
     if(max_overlap == 2) return 2 * n - 2;
     if(max_overlap > chessboard_num) return 1;
     
-    vector<pii> results(max_overlap);
-    int max_dist = -1, target_dist;
-    vector<pii> cur_able_points;
-    for(int r = 0; r<n; r++){
-        for(int c= 0; c<n; c++){
-            cur_able_points.push_back({r, c});
-        }
-    }
+    int target_dist;
     for(target_dist = 2*n-3; target_dist >= 3; target_dist--){
-        bool flag = backtrack(0, max_overlap, target_dist, results, cur_able_points, n);
-        if(flag) break;
+        for(int rs = 0; rs<n; rs++){
+            for(int cs = 0; cs<n; cs++){
+                // {rs, cs} : 탐색 시작점 / row_start, col_start
+                vector<pii> lockers;
+                lockers.push_back({rs, cs});
+
+                int max_people = -1;
+
+                // row_start보다 위에는 넣지 않겠음.
+                for(int cr = rs; cr<n; cr++){
+                    for(int cc = 0; cc<n; cc++){
+                        // {cr, cc} : 현재 보는 위치 / current_row, current_col
+                        bool canInsert = true;
+                        for(int lockeridx = 0; lockeridx < lockers.size(); lockeridx++){
+                            if(getDist(lockers[lockeridx], {cr, cc}) < target_dist){
+                                canInsert = false;
+                                break;
+                            }
+                        }
+
+                        if(canInsert == false){
+                            continue;
+                        }
+                        lockers.push_back({cr, cc});
+                    }
+                }
+                if(max_overlap == lockers.size()) return target_dist;
+            }
+        }
     }
     
     
