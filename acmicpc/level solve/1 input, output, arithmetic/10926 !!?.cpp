9f4f86ce0d5da159ce0bff8e02d6554b3e5d5d21
hyelie <hyelie@postech.ac.kr>
2022-06-22 09:17:26 +0000

    Solve: 22.06.02. 10926.cpp

---
+++ acmicpc/level solve/1 input, output, arithmetic/10926 !!?.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve();
+
+int main(void) {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+    string s;
+	cin>>s;
+    cout<<s + "??!";
+	
+	return 0;
+}
