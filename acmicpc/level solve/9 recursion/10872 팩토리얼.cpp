43b3e99f450239e555d4658cebe48eef664016a4
hyelie <hyelie@postech.ac.kr>
2022-06-25 10:19:01 +0000

    Solve : 10872 팩토리얼

---
+++ acmicpc/level solve/9 recursion/10872 팩토리얼.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+
+int recurse(int N){
+	if(N <= 1) return 1;
+	else return recurse(N-1) * N;
+}
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	int N; cin>>N;
+	cout<<recurse(N);
+
+	return 0;
+}
