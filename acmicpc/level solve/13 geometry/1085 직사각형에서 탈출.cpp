1a3a22dfcf946ec7508f8818270f1fbf102f54f9
hyelie <hyelie@postech.ac.kr>
2022-06-26 14:00:23 +0000

    Solve : 1085 직사각형에서 탈출

---
+++ acmicpc/level solve/13 geometry/1085 직사각형에서 탈출.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+
+	int x, y, w, h;
+	cin>>x>>y>>w>>h;
+	cout<<min(min(x, w-x), min(y, h-y));
+
+	//////////////////////
+
+	return 0;
+}
