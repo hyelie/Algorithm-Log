8beca999245b9cc3e26aaacb503a5b9df68f1f5f
hyelie <hyelie@postech.ac.kr>
2022-06-26 14:13:42 +0000

    Solve : 3053 HERMAN

---
+++ acmicpc/level solve/13 geometry/3053 HERMAN.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+
+	double R; cin>>R;
+	cout<<fixed;
+	cout.precision(10);
+	cout<<M_PI * R * R<<'\n';
+	cout<<2*R*R;
+
+	//////////////////////
+
+	return 0;
+}
