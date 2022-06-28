005eb77bcde32757d918e0e2c48359fc664077ff
hyelie <hyelie@postech.ac.kr>
2022-06-28 10:16:09 +0000

    Solve : 11650 좌표 정렬하기

---
+++ acmicpc/level solve/11 sort/11650 좌표 정렬하기.cpp
@@ -0,0 +1,45 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+bool cmp(pii &a, pii &b){
+	if(a.first == b.first) return a.second < b.second;
+	else return a.first < b.first;
+}
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+	
+	int N; cin>>N;
+	vector<pii> v(N);
+	int x, y;
+	for(int i = 0; i<N; i++){
+		cin>>v[i].first>>v[i].second;
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for(pii point : v){
+		cout<<point.first<<' '<<point.second<<'\n';
+	}
+
+	//////////////////////
+
+	return 0;
+}
