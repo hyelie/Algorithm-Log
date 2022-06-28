cf55298bf6c49e06cb559123af2d998ad5348d6a
hyelie <hyelie@postech.ac.kr>
2022-06-28 10:14:01 +0000

    Solve : 1427 소트인사이드

---
+++ acmicpc/level solve/11 sort/1427 소트인사이드.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+	
+	int N;
+	string s; cin>>s;
+	sort(s.begin(), s.end(), greater<char>());
+	cout<<s;
+
+	//////////////////////
+
+	return 0;
+}
