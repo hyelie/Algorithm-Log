f4e3ff7e52590738060a3bca55c875c94449cf27
hyelie <hyelie@postech.ac.kr>
2022-07-01 00:26:15 +0000

    Set Token Authorization

--- template_multi.cpp
+++ template_multi.cpp
@@ -1,28 +1,39 @@
+#define _USE_MATH_DEFINES 
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <set>
 
 using namespace std;
+typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<int, string> pis;
-typedef pair<string, int> psi;
 
-void solve();
+void solve(){
+	
+}
 
 int main(void) {
 	cin.tie(0);
+	cout.tie(0);
 	std::ios_base::sync_with_stdio(0);
-	int test;
-	cin >> test;
-	while (test--) {
+
+	////////////////////// write your code below
+	
+	int t; cin>>t;
+	while(t--){
 		solve();
 	}
+
+
+
+	//////////////////////
+
 	return 0;
-}
-
-void solve() {
-	cout << "hello, world!" << endl;
-
-	return;
 }
