985f8643609424606a14e940329fb750a420f749
hyelie <hyelie@postech.ac.kr>
2022-07-04 12:59:14 +0000

    Solve BOJ 15650 N과 M (2)

---
+++ acmicpc/level solve/15 backtracking/15650 N과 M (2).cpp
@@ -0,0 +1,52 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+
+vector<int> nums;
+int N, M; 
+
+void combination(int cur_depth, int max_depth, int prev_idx, vector<int>& result){
+	if(cur_depth == max_depth){
+		for(int i : result) cout<<i<<' ';
+		cout<<'\n';
+		return;
+	}
+	for(int i = prev_idx + 1; i<N; i++){
+		result[cur_depth] = nums[i];
+		combination(cur_depth + 1, max_depth, i, result);
+	}
+}
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+	
+	cin>>N>>M;
+	for(int i = 1; i<=N; i++){
+		nums.push_back(i);
+	}
+	vector<int> result(M);
+
+	permutation(0, M, -1, result);
+
+
+	//////////////////////
+
+	return 0;
+}
