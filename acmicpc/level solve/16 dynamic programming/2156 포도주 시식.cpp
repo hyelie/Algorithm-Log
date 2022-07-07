37e06f3d3a75f6b34d8c3a36c4e442c207958337
hyelie <hyelie@postech.ac.kr>
2022-07-07 14:31:35 +0000

    Solve 2156 포도주 시식 Logic 추가

--- acmicpc/level solve/16 dynamic programming/2156 포도주 시식.cpp
+++ acmicpc/level solve/16 dynamic programming/2156 포도주 시식.cpp
@@ -26,12 +26,26 @@ int main(void) {
 	vector<int> drinks(n+1), dp(n+1, 0);
 	for(int i = 1; i<=n; i++) cin>>drinks[i];
 
+	////////////// logic 1
 	dp[1] = drinks[1];
 	if(n >= 2) dp[2] = drinks[1] + drinks[2];
+	if( n >= 3) dp[3] =max(drinks[3] + drinks[2], drinks[1] + drinks[3]);
 	// dp[i] : i번째 잔을 마셨을 때 최대값
 
+	for(int i = 4; i<=n; i++){
+		dp[i] = max(max(dp[i-2] + drinks[i], dp[i-3] + drinks[i-1] + drinks[i]), dp[i-4] + drinks[i-1] + drinks[i]);
+	}
+
+	///////////// logic 2
+	dp[1] = drinks[1];
+	if(n >= 2) dp[2] = drinks[1] + drinks[2];
+	// dp[i] : i번째 잔까지 봤을 때 최대값
+	//		i번째를 마시지 않거나
+	//		i-2번째 + i번째거나
+	//		i-3번째 + i-1번째 + i번째거나
+
 	for(int i = 3; i<=n; i++){
-		dp[i] = max(dp[i-1], max(dp[i-2] + drinks[i], dp[i-3] + drinks[i-1] + drinks[i]));
+		dp[i] = max(max(dp[i-2] + drinks[i], dp[i-3] + drinks[i-1] + drinks[i]), dp[i-1]);
 	}
 	
 	cout<<*max_element(dp.begin(), dp.end());
