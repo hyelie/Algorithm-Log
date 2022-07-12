e9c5f97fd7e2f45a3e9218dab2b0576595f732b8
hyelie <hyelie@postech.ac.kr>
2022-07-12 13:45:41 +0000

    Solve BOJ 18258 큐 2

---
+++ acmicpc/level solve/20 queue, deque/18258 큐 2.cpp
@@ -0,0 +1,66 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <deque>
+#include <cmath>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+
+int main(void) {
+	cin.tie(0);
+	cout.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	////////////////////// write your code below
+	
+	int N; cin>>N;
+	string command; int input;
+	deque<int> q;
+	while(N--){
+		cin>>command;
+		if(command == "push"){
+			cin>>input;
+			q.push_back(input);
+		}
+		else if(command == "pop"){
+			if(q.empty()) cout<<"-1\n";
+			else{
+				cout<<q.front()<<'\n';
+				q.pop_front();
+			}
+		}
+		else if(command == "size"){
+			cout<<q.size()<<'\n';
+		}
+		else if(command == "empty"){
+			cout<<q.empty()<<'\n';
+		}
+		else if(command == "front"){
+			if(q.empty()) cout<<"-1\n";
+			else{
+				cout<<q.front()<<'\n';
+			}
+		}
+		else if(command == "back"){
+			if(q.empty()) cout<<"-1\n";
+			else{
+				cout<<q.back()<<'\n';
+			}
+		}
+	}
+
+	//////////////////////
+
+	return 0;
+}
