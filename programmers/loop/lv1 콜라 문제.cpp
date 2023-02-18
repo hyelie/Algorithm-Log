8d527f3fa6a8e814f5ac526e46245eb2359a5c3f
hyelie <hyelie@postech.ac.kr>
2023-02-18 10:45:02 +0000

    Solve Programmers 콜라 문제

---
+++ programmers/loop/lv1 콜라 문제.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while(n >= a){
+        answer += n/a*b;
+        n = n/a*b + n%a;
+    }
+    
+    return answer;
+}
+
+/*
+n / a, 
+n = n/a + b
+*/
