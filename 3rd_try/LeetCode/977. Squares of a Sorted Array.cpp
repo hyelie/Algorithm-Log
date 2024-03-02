a66c5905b5cf49e95521df4c45005430695e9227
hyelie <hyelie@postech.ac.kr>
2024-03-02 22:13:07 +0900

    solve: Leetcode 977. Squares of a Sorted Array Easy, 30초

---
+++ 3rd_try/LeetCode/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int &num : nums) num *= num;
+        sort(nums.begin(), nums.end(), less<int>());
+        return nums;
+    }
+};
